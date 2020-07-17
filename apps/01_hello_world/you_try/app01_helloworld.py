4ddf20c7e7bd236543254844201a8994df5e3021
Micheal Mac Donnacha <micheal.macdonnacha13@gmail.com>
2020-07-17 20:14:51 +0100

    Adding app 01 Hello World

--- apps/01_hello_world/you_try/app01_helloworld.py
+++ apps/01_hello_world/you_try/app01_helloworld.py
@@ -1,5 +1,7 @@
 print('---------------------------------')
 print('           HELLO APP')
 print('---------------------------------')
+
+
 name = input('What is your name ? ')
 print('Nice to meet you ' + name)
